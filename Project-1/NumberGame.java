c082c711d4dfcc0c82801e98d650e957c1a82a5f
Divyansh Kaushik <dk.tech2007@gmail.com>
2026-06-16 18:17:23 +0530

    StudentGradeCalculator

--- Project-1/NumberGame.java
+++ Project-1/NumberGame.java
@@ -1,28 +1,22 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class NumberGame {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
-
         int score = 0;
         char playAgain;
-
         do {
             int secretNumber = random.nextInt(100) + 1;
             int guess;
             int attempts = 0;
-
             System.out.println("\n=== Number Guessing Game ===");
             System.out.println("I have selected a number between 1 and 100.");
             System.out.println("Try to guess it!");
-
             while (true) {
                 System.out.print("Enter your guess: ");
                 guess = sc.nextInt();
                 attempts++;
-
                 if (guess > secretNumber) {
                     System.out.println("Too High! Try again.");
                 } else if (guess < secretNumber) {
@@ -34,17 +28,12 @@ public class NumberGame {
                     break;
                 }
             }
-
             System.out.println("Current Score: " + score);
-
             System.out.print("Do you want to play another round? (Y/N): ");
             playAgain = sc.next().charAt(0);
-
         } while (playAgain == 'Y' || playAgain == 'y');
-
         System.out.println("\nFinal Score: " + score);
         System.out.println("Thank you for playing!");
-
         sc.close();
     }
 }
---
+++ Project-2/StudentGradeCalculator.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int subjects = sc.nextInt();
+        int totalMarks = 0;
+        for (int i = 1; i <= subjects; i++) {
+            int marks;
+            do {
+                System.out.print("Enter marks for Subject " + i + " (0-100): ");
+                marks = sc.nextInt();
+                if (marks < 0 || marks > 100) {
+                    System.out.println("Invalid marks! Please enter between 0 and 100.");
+                }
+            } while (marks < 0 || marks > 100);
+            totalMarks += marks;
+        }
+        double percentage = (double) totalMarks / subjects;
+        String grade;
+        if (percentage >= 90) {
+            grade = "A+";
+        } else if (percentage >= 80) {
+            grade = "A";
+        } else if (percentage >= 70) {
+            grade = "B";
+        } else if (percentage >= 60) {
+            grade = "C";
+        } else if (percentage >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+        System.out.println("\n===== RESULT =====");
+        System.out.println("Total Marks: " + totalMarks + "/" + (subjects * 100));
+        System.out.printf("Average Percentage: %.2f%%\n", percentage);
+        System.out.println("Grade: " + grade);
+        if (percentage >= 40) {
+            System.out.println("Status: PASS");
+        } else {
+            System.out.println("Status: FAIL");
+        }
+        sc.close();
+    }
+}
